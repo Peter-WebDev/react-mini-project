b97fce8d19e78c70d1da519721e05eb79b23dc2f
Peter <peter.warholm@gmail.com>
2025-02-20 19:27:07 +0100

    Added conditional rendering for the src issue, added two styled components, CardImg and Placeholder

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,38 @@
 import { Link } from "react-router";
+import styled from "styled-components";
 import { Movie } from "../data/api";
+
+const CardImg = styled.img`
+    max-width: 100%;
+    display: block;
+`;
+
+const Placeholder = styled.div`
+    width: 100%;
+    height: 400px;
+    background-color: #eee;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #888;
+`;
 
 interface Props {
     movie: Movie;
 }
 
 export default function Card(props: Props) {
+    // Conditinal rendering instead to check if there is a poster or not
     return (
         <Link to={`movies/${props.movie.title}/${props.movie.id}`}>
             <div>
-                <img src="" alt="" />
+                {props.movie.poster_path ? (
+                    <CardImg src={props.movie.poster_path} alt={props.movie.title || "Poster"} />
+                ) : (
+                    <Placeholder>
+                        No poster available
+                    </Placeholder>
+                )}
                 <div>
                     <h3>Movie title</h3>
                     <p>Year</p>
