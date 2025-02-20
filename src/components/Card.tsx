8401892949f3c4127a9820e2b203e7ae04eb453d
Peter <peter.warholm@gmail.com>
2025-02-20 19:08:17 +0100

    Renamed MovieCard to Card for reusablility, Added initial function Card with basic strutcure

---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+export default function Card(prop: Props) {
+    return (
+        <div>
+            <img src="" alt="" />
+            <div>
+                <h3>Movie title</h3>
+                <p>Year</p>
+            </div>
+        </div>
+    )
+}
