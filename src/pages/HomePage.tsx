078ef0a70128a612e2be73d36c249e435a534099
Peter <peter.warholm@gmail.com>
2025-02-22 16:36:34 +0100

    Added some CardList components to render movies

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,13 @@
+import CardList from "../components/CardList";
+
 export default function HomePage() {
     return (
-        <h1>HomePage</h1>
-    )
+        <div>
+            <h1>Welcome to Popcorn+</h1>
+            <CardList categoryName="Action" genreId={28} numberOfMovies={15} categoryColor="rgb(240, 69, 21)" />
+            <CardList categoryName="Comedy" genreId={35} numberOfMovies={15} categoryColor="rgb(255, 255, 0)" />
+            <CardList categoryName="Drama" genreId={18} numberOfMovies={15} categoryColor="rgb(255, 182, 193)" />
+            <CardList categoryName="Science Fiction" genreId={878} numberOfMovies={15} categoryColor="rgb(0, 255, 255)" />
+        </div>
+    );
 }
