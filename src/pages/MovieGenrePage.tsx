f4c65087fe66c6aa325e2764af3479c65db14fe4
Peter <peter.warholm@gmail.com>
2025-02-24 17:37:03 +0100

    Added useState for handling loading more movies and useQuery to fetch and refetch movies

--- src/pages/MovieGenrePage.tsx
+++ src/pages/MovieGenrePage.tsx
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { useParams } from "react-router";
+import { fetchMoviesByGenre, Movie } from "../data/api";
 
 export default function MovieGenrePage() {
   const params = useParams();
@@ -6,13 +9,22 @@ export default function MovieGenrePage() {
 
 
   if (!categoryId) {
-    return <h1>Loading...</h1>;
+    return <p>Category ID is missing</p>;
   }
 
   // Small regexpression to check if the categoryId is a valid number
   if (!/^\d+$/.test(categoryId)) {
     return <h1>Invalid Category ID</h1>;
   }
+
+  const genreId = Number(categoryId);
+
+  const [numberOfMoviesToLoad, setNumberOfMoviesToLoad] = useState(15);
+
+  const { isLoading, error, data, refetch } = useQuery<Movie[], Error>({
+    queryKey: ['movies', genreId, numberOfMoviesToLoad],
+    queryFn: () => fetchMoviesByGenre(genreId, numberOfMoviesToLoad),
+  });
 
   return (
     <>
