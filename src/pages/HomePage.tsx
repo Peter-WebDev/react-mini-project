015764fe9e46896a7fe1f4b819b845d2dcf9c9af
Peter <peter.warholm@gmail.com>
2025-02-24 21:36:39 +0100

    Styling for the genre name on hover and focus-visible, changing SciFi color slightly

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,31 +6,37 @@ import CardList from "../components/CardList";
 const GenreContainer = styled(Link)<{ $categoryColor: string }>`
     display: flex;
     align-items: center;
-    margin-bottom: 2rem;
+    gap: 0.5rem;
+    margin-top: 2rem;
     text-decoration: none;
     border-bottom: 2px solid ${({ $categoryColor }) => $categoryColor};
-    padding-bottom: 1rem;
+    padding-bottom: 0.5em;
 `;
 
 // Added categoryColor from CardList
 const GenreTitle = styled.h2<{ $categoryColor: string }>`  // Add type for categoryColor
   display: inline-block;
-  transition: color 0.3s ease;
+  transition: color 0.4s ease-in-out;
   color: ${({ $categoryColor }) => $categoryColor}; // Set color based on prop
 
-  &:hover {
-    color: #FBFBFB;
+  ${GenreContainer}:hover &,
+  ${GenreContainer}:focus-visible & {
+    color: ${({ $categoryColor }) => `rgba(${$categoryColor.slice(4, -1)}, 0.8)`};
   }
 `;
 
 const SeeMoreLink = styled.span`
     cursor: pointer;
     visibility: hidden; // Initially hidden
+    transition: color 0.4s ease-in-out;
+    color: transparent;
 
-    ${GenreTitle}:hover & { // Show on GenreTitle hover
+    ${GenreContainer}:hover &,
+    ${GenreContainer}:focus-visible & { // Show on GenreTitle hover
         visibility: visible;
+        color: #4ed5e4;
     }
-`
+`;
 
 // Added from CardList
 const CardListWrapper = styled.div`
@@ -49,7 +55,7 @@ const genres = [
     { id: 28, name: 'Action', color: 'rgb(240, 69, 21)'},
     { id: 35, name: 'Comedy', color: 'rgb(255, 255, 0)'},
     { id: 18, name: 'Drama', color: 'rgb(255, 182, 193)'},
-    { id: 878, name: 'Science Fiction', color: 'rgb(0, 255, 255)'},
+    { id: 878, name: 'Science Fiction', color: 'rgb(130, 185, 155)'},
 ];
 
 
