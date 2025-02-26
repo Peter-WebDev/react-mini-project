c92ca6cbfaaaf57a3e1a99893c71a0b8ef31f126
Peter <peter.warholm@gmail.com>
2025-02-26 23:09:25 +0100

    Added code for rendering the liked movies with useLikedMovies + changed minmax in grid

--- src/pages/MyFavPage.tsx
+++ src/pages/MyFavPage.tsx
@@ -1,6 +1,26 @@
+import styled from "styled-components";
+import Card from "../components/Card";
+import { useLikedMovies } from "../providers/LikedMoviesProvider";
+
+// Same grid-setup as HomePage and MovieGenrePage
+const CardListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(175px, 1fr));
+  gap: 1rem;
+  margin-top: 1rem;
+`;
+
 export default function MyFavPage() {
+    const { likedMovies } = useLikedMovies();
 
     return (
-        <h1>My list of favourite movies</h1>
-    )
+        <>
+            <h1>My list of favourite movies</h1>
+            <CardListWrapper>
+                {likedMovies.map(movie => (
+                    <Card key={movie.id} movie={movie} />
+                ))}
+            </CardListWrapper>
+        </>
+    );
 }
