df70de324ee52a311d134c61a92b755c9d353e29
Peter <peter.warholm@gmail.com>
2025-02-26 11:20:35 +0100

    Created context, provider and toggle function for liking movies

--- src/providers/LikedMoviesProvider.tsx
+++ src/providers/LikedMoviesProvider.tsx
@@ -1,21 +1,31 @@
 import { createContext, PropsWithChildren, useContext, useState } from "react";
+import { Movie } from "../data/api";
 
+interface LikedMoviesValue {
+    likedMovies: Movie[];
+    toggleLikedMovie: (movie: Movie) => void;
+    isMovieLiked: (movieId: number) => boolean;
+}
 
-const LikedMoviesContext = createContext({} as LikedMovieValue);
+const LikedMoviesContext = createContext({} as LikedMoviesValue);
 
 export default function LikedMoviesProvider(props: PropsWithChildren) {
-    const [likedMovies, setLikedMovies] = useState<string[]>([]);
+    const [likedMovies, setLikedMovies] = useState<Movie[]>([]);
 
-    const toggleLikedMovie = (id: string) => {
-        if (likedMovies.includes(id)) {
-            setLikedMovies(likedMovies.filter((movieId) => movieId !== id));
+    const toggleLikedMovie = (movie: Movie) => {
+        if (isMovieLiked(movie.id)) {
+            setLikedMovies(likedMovies.filter((m) => m.id !== movie.id));
         } else {
-            setLikedMovies([...likedMovies, id]);
+            setLikedMovies([...likedMovies, movie]);
         }
     };
 
+    const isMovieLiked = (movieId: number) => {
+        return likedMovies.some((movie) => movie.id === movieId);
+    }
+
     return (
-        <LikedMoviesContext.Provider value={{ likedMovies, toggleLikedMovie }}>
+        <LikedMoviesContext.Provider value={{ likedMovies, toggleLikedMovie, isMovieLiked }}>
             {props.children}
         </LikedMoviesContext.Provider>
     );
