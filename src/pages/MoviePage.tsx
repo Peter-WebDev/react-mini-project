11ad33dbc3b929f0b490fdff7cbf5903f74d5dd1
Peter <peter.warholm@gmail.com>
2025-02-28 00:09:23 +0100

    Added styling + added handling if no data / information is available

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -33,10 +33,10 @@ const InfoContainer = styled.header`
 `;
 
 const Title = styled.h1`
-    margin-bottom: 0.5rem;
+    /* margin-bottom: 0.5rem; */
 `;
 
-const TagLine = styled.p`
+const TagLine = styled.q`
     font-style: italic;
     margin-bottom: 1rem;
 `;
@@ -58,6 +58,28 @@ const Rating = styled.div`
     font-weight: 700;
 `;
 
+const RatingNumber = styled.span`
+    color: rgb(16, 196, 0);
+`;
+
+const VoteNumber = styled.span`
+    color: rgb(214, 161, 14);
+`;
+
+const MoviePoster = styled.img`
+    border-radius: 0.5rem;
+`;
+
+const Url = styled.a`
+    text-decoration: none;
+    color: rgb(78, 213, 228);
+
+    &:hover,
+    &:focus {
+        text-decoration: underline;
+    }
+`;
+
 export default function MoviePage() {
     const { id } = useParams<{ id: string }>();
 
@@ -76,26 +98,44 @@ export default function MoviePage() {
         return <p>Error: Could not load movies.</p>
     }
 
+    // Fix for new Date since new Date can't be (undefined) or (null)
+    const releaseDate: Date = new Date(movie?.release_date ?? "");
+
+    if (isNaN(releaseDate.getTime())) {
+        console.log("invalid Date");
+    } else {
+        console.log(releaseDate);
+    }
+
     return (
         <>
             <ArticleContainer>
                 <MovieContainer>
-                {movie.poster_path ? (
-                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" width="342" height="513" />
+                {movie?.poster_path ? (
+                    <MoviePoster src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" width="500" height="750" />
                 ) : (
                     <Placeholder>
                         No poster available
                     </Placeholder>
                 )}
                     <InfoContainer>
-                        <Title></Title>
-                        <TagLine></TagLine>
-                        <ReleaseDate></ReleaseDate>
-                        <Overview></Overview>
+                        <Title>{movie?.title}</Title>
+                        {movie?.tagline ? (
+                            <TagLine>{movie?.tagline}</TagLine>
+                        ) : (
+                            <i>No tagline available</i>
+                        )}
+                        <Overview>{movie?.overview}</Overview>
+                        <ReleaseDate>Release Date: {new Date(releaseDate).toLocaleDateString()}</ReleaseDate>
                         <Ratings>
-                            <Rating></Rating>
-                            <Rating></Rating>
+                            <Rating>Rating: <RatingNumber>{movie?.vote_average.toFixed(1)}</RatingNumber>/10</Rating>
+                            <Rating>Votes: <VoteNumber>{movie?.vote_count.toLocaleString()}</VoteNumber></Rating>
                         </Ratings>
+                        {movie?.homepage && (
+                            <Url href={movie.homepage} target="_blank" rel="noopener noreferrer">
+                                Vistit the Movie's Homepage
+                            </Url>
+                        )} {!movie?.homepage && <i>Homepage not available</i>}
                     </InfoContainer>
                 </MovieContainer>
             </ArticleContainer>
